c00d2979a571c20cd4109f89a7f5cae459854f44
Paul Keno <adarojepaulo@gmail.com>
2023-02-21 14:20:04 +0100

    changes

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-const form = document.getElementById("form");
 
-//grab all input fields
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
-const user = document.getElementById("user");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const bio = document.getElementById("bio");
+//get all input fields
+const firstName = document.getElementById("first-name")
+const lastName = document.getElementById("last-name")
+const user = document.getElementById("user")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const bio = document.getElementById("bio")
 
-let data =[]
-data = JSON.parse(localStorage.getItem("user")) || []
+//initiate data model
+let data = [];
+data = JSON.parse(localStorage.getItem("account")) || [];
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault()
-    const account = new Account(firstName.value, lastName.value, user.value, password.value, bio.value )
-   
 
-    const checkResult = checkUser(account.email)
-    console.log(checkResult)
+//add event listener to form
+const form = document.getElementById("form")
 
-    if(checkResult){
-        alert("User already exist!!!")
-        throw new Error("User already exist!!!")
+form.addEventListener("submit", (event)=> {
+    event.preventDefault()
+
+    //create an account instance
+    const userAccount = new Account(
+        firstName.value,
+        lastName.value,
+        user.value,
+        email.value,
+        password.value,
+        bio.value,
+    )
+
+    //check if account exist
+    const emailCheck = userAccount.doesAccountExist(userAccount.email) 
+
+    if(emailCheck){
+        alert("Account already exist!!!");
+        throw new Error("Account already exist!!!")
     }else{
-        data.push(account);
-        localStorage.setItem("user", JSON.stringify(data))
-        console.log(data)
+        data.push(userAccount);
+        localStorage.setItem("account", JSON.stringify(data))
     }
-
-
-    //check if account exists ---->>>> get all account, find if account exist... push or throw an error
 
 })
 
-//check if user exist
-const checkUser = (email) => {
-    console.log(user)
-    if(!data) return
-    //find method
-    const check = data.find(value => value.email === email)
-    console.log(check)
-   return check
-
-    
+//get all accounts
+const account = document.getElementById("account")
+function getAllAccount(){
+    let value = ""
+     data.map((item) => {
+     value += `
+        <div class="card">
+            <p> Name: ${item?.firstName} ${item?.lastName}</p>
+            <p>Email: ${item?.email}  </p>
+            <p>Username: ${item?.email}</p>
+            <p>Bio: ${item?.bio}</p>
+        </div>
+        `
+    })
+    account.innerHTML = value
 }
 
-//OOP
+getAllAccount()
+
 class Account {
-    constructor(firstName, lastName, user, password, bio ){
+    constructor(firstName, lastName, user, email, password, bio ){
         this.firstName = firstName;
         this.lastName = lastName;
         this.user = user;
+        this.email = email;
         this.password = password;
         this.bio = bio;
+    }
+
+    doesAccountExist(email){
+        if(!data) return
+        const check = data.find(value => value.email === email)
+        return check
+    }
+
+    getAllAccount(){
+        return data
     }
 }
